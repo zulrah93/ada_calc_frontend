bbdbb670ede7e13cc192e1544efee0041388d0b4
zulrah <none@yourbusiness.fu>
2022-05-19 22:20:01 -0400

    Added options from CLI backend, added style sheets, added js scripts, added favicon, still some WIP with look and feel looking like it is web 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
 
+    hljs.highlightAll(); // Ensure script tag is on the HTML side; used to enable syntax highlighting on the input
+
+    $('#verbose_flag').change(function() {
+        if(this.checked) {
+            $('#verbose').prop('value', 'true');
+        }
+        else {
+            $('#verbose').prop('value', 'false');
+        }
+    });
+
+    $('#graph_flag').change(function() {
+        if(this.checked) {
+            $('#graph').prop('value', 'true');
+        }
+        else {
+            $('#graph').prop('value', 'false');
+        }
+    });
+
+    // Always update the json hidden view using the text from the json editor in real time -- ⚠️ might optimize if it impacts browser performance
+    $('#json_editor').bind('click keypress mousedown', function() {
+          $('#json_view').text($('#json_editor').text());
+    });
 
 });
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use actix_web::http::StatusCode;
 use actix_web::web::{Data, Form};
 use actix_web::Result;
-use actix_web::{web, get, App, HttpRequest,  HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use std::fs::read_to_string;
 use std::process::Command;
 
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 struct Pool {
     json: String,
-    verbose : String,
-    graph : String,
+    verbose: bool,
+    graph: bool,
 }
 
 #[get("/")]
@@ -19,27 +19,47 @@ async fn index() -> Result<HttpResponse> {
         .content_type("text/html; charset=utf-8")
         .body(format!(
             include_str!("../static/index.html"),
-            format!("<b>{}</b>", "Nothing Calculated Yet")
+            format!("<b style='color:red'>{}</b>", "Nothing Calculated Yet")
         )))
 }
 
 #[get("/scripts/index.js")]
-async fn js() -> Result<HttpResponse> {
+async fn index_js() -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/javascript; charset=utf-8")
-        .body(include_str!("../scripts/index.js")
-        ))
+        .body(include_str!("../scripts/index.js")))
+}
+
+#[get("/vectors/favicon.svg")]
+async fn favicon() -> Result<HttpResponse> {
+    // Favorite Icon in SVG format
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("image/svg+xml; charset=utf-8")
+        .body(include_str!("../vectors/cardano.svg")))
+}
+
+#[get("/styles/index.css")]
+async fn index_css() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/css; charset=utf-8")
+        .body(include_str!("../styles/index.css")))
 }
 
 // Spawns a child process -- this function will never return None so it is safe to unwrap
-// TODO: Implement verbose and graph flags
-fn execute_ada_calc_backend(json: String, _verbose : bool, _graph : bool, cli_path: String) -> Option<String> {
-    if let Ok(output) = Command::new(cli_path)
-        .args(["-p", json.as_str()])
-        .args(["-v"]) // Verbose output
-        .args(["-G"]) // Generate CSV
-        .output()
-    {
+fn execute_ada_calc_backend(
+    json: String,
+    verbose: bool,
+    graph: bool,
+    cli_path: String,
+) -> Option<String> {
+    let mut args = vec![format!("-p {}", json)];
+    if verbose {
+        args.push(String::from("-v"));
+    }
+    if graph {
+        args.push(String::from("-G"));
+    }
+    if let Ok(output) = Command::new(cli_path).args(args).output() {
         Some(
             String::from_utf8(output.stdout.into_iter().collect::<Vec<u8>>())
                 .unwrap_or(String::from("JSON Invalid!")),
@@ -49,15 +69,21 @@ fn execute_ada_calc_backend(json: String, _verbose : bool, _graph : bool, cli_pa
     }
 }
 
-async fn calculate(_request: HttpRequest, data: Data<String>, form: Form<Pool>) -> impl Responder {
-    println!("Graph On: {} Verbose On: {}", form.graph, form.verbose);
+async fn calculate(_request: HttpRequest, data: Data<String>, pool: Form<Pool>) -> impl Responder {
+
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(format!(
             include_str!("../static/index.html"),
             format!(
-                "<b>{}</b>",
-                execute_ada_calc_backend(form.json.clone(), true, true, data.to_string()).unwrap()
+                "<b style='color:blue'>{}</b>",
+                execute_ada_calc_backend(
+                    pool.json.clone(),
+                    pool.verbose,
+                    pool.graph,
+                    data.to_string()
+                )
+                .unwrap()
             )
         ))
 }
@@ -95,15 +121,17 @@ async fn main() -> std::io::Result<()> {
         );
         default_config
     };
+    //TODO: Implement HTTPS with a signed certificate to ensure privacy of information being calculated
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(config.ada_calc_backend_path.clone())) // Pass backend path
-            .service(js)
+            .service(index_js)
+            .service(favicon)
+            .service(index_css)
             .service(index)
             .service(web::resource("/calculate").route(web::post().to(calculate)))
-            
     })
     .bind((config.host_or_ip, config.port))?
     .run()
